0c618eb487fef122deda58f7e9f61080ffc55b8c
tesztsor-ta <tesztsor-ta@fnadas.net>
2025-03-23 22:30:04 +0000

    remove logging

--- certbot-renew.sh
+++ certbot-renew.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# certbot-renew.sh
 set -e
-
 cd "$(dirname "$0")"
-
-echo "[+] Starting Certbot renewal with webroot..."
-
-docker compose run --rm certbot renew \
-  --webroot --webroot-path=/var/www/certbot
-
-echo "[+] Reloading Nginx to apply renewed certs..."
+docker compose run --rm certbot renew --webroot --webroot-path=/var/www/certbot
 docker compose exec nginx nginx -s reload
-
-echo "[+] Done."
 
